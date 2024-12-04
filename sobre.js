ae267e9d2677f3b828c93a2e37bcc7b24269157b
Larita <160742572+djulia-jj@users.noreply.github.com>
2024-12-04 18:49:36 -0300

    negrito

--- sobre.js
+++ sobre.js
@@ -12,7 +12,7 @@ class Texto extends React.Component {
           <br/>Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.<br/>
           <br/>Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).<br/>
           <br/>O IFSP é organizado em estrutura multicampi e possui aproximadamente 24 mil alunos matriculados nos 38 campi e mais 4 mil alunos nos 19 polos de educação a distância distribuídos pelo estado de São Paulo.<br/>
-          <br/>Localizado em {this.props.endereco}<br/>
+          <br/>Localizado em <strong>{this.props.endereco}</strong><br/>
         </p>
       );
     }
