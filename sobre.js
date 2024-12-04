2d88ebe8d197bb5cd5bf31d0f69521b9019bdbc0
Larita <160742572+djulia-jj@users.noreply.github.com>
2024-12-04 18:58:08 -0300

    smlsd

--- sobre.js
+++ sobre.js
@@ -5,8 +5,7 @@ class Texto extends React.Component {
     render() {
       return (
         <p>
-          O <strong>{this.props.negrito}</strong> – é uma autarquia federal de ensino.
-          <br/>
+          O <strong>{this.props.negrito}</strong> – é uma autarquia federal de ensino.<br/>
           <br/>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.<br/>
           <br/>Durante seus 105 anos de história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.<br/>
           <br/>Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.<br/>
